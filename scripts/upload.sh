781289351f9696abaf5f3531068768aec97c2ac1
Sirius Taikun <siriustaikun@protonmail.com>
2023-05-05 21:32:19 -0600

    add entrypoints

---
+++ scripts/upload.sh
@@ -0,0 +1,7 @@
+junod tx wasm store ./artifacts/nft_auction.wasm \
+  --node https://juno-rpc.polkachu.com:443 \
+  --chain-id juno-1 --from flix-pupmos-burner \
+  --gas-prices 1.5ujuno --gas 5000000 \
+  --gas-adjustment 1.5 -y -b block
+
+  # 899.9K $TOKEN
